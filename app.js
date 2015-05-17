dcdd2bba4b6e607c012835cdd2db1aaddf9bb56c
Alexis THOMAS <fr.alexisthomas@gmail.com>
2015-05-17 10:47:17 +0200

    Add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+// Annotation section
+@Component({
+    selector: 'my-app'
+})
+@View({
+    inline: '<h1>Hello {{ name }}</h1>'
+})
+// Component controller
+class MyAppComponent {
+    constructor() {
+        this.name = 'Alice'
+    }
+}
+bootstrap(MyAppComponent)
